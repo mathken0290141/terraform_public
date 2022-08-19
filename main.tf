55b3878c8ae8fba4b1963d6a9b1091505e2a26fe
@_mathken <dummy@example.com>
2022-08-19 14:22:30 +0900

    有効化するためのパラメータを追加しました。

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ POLICY
 }
 
 # (AWS Config) 有効化（レコーダーの作成）【MUST】
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/config_config_rule
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/config_configuration_recorder_status
 
 resource "aws_config_config_rule" "r" {
   name = "rakulogi"
@@ -119,6 +119,33 @@ resource "aws_iam_role_policy" "p" {
           "Resource": "*"
 
       }
+  ]
+}
+POLICY
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/config_configuration_recorder
+
+resource "aws_config_configuration_recorder" "rakulogi" {
+  name     = "rakulogi"
+  role_arn = aws_iam_role.r.arn
+}
+
+resource "aws_iam_role" "r" {
+  name = "awsconfig-rakulogi"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "config.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
   ]
 }
 POLICY

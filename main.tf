028f2c0f62e0c9e3d32fae93db449e9f7dfd7176
@_mathken <dummy@example.com>
2022-08-19 14:14:57 +0900

    AWS Configの有効化自体は必要だったため追加しました。

--- main.tf
+++ main.tf
@@ -66,3 +66,60 @@ resource "aws_s3_bucket_policy" "rakulogi" {
 POLICY
 }
 
+# (AWS Config) 有効化（レコーダーの作成）【MUST】
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/config_config_rule
+
+resource "aws_config_config_rule" "r" {
+  name = "rakulogi"
+
+  source {
+    owner             = "AWS"
+    source_identifier = "S3_BUCKET_VERSIONING_ENABLED"
+  }
+
+  depends_on = [aws_config_configuration_recorder.rakulogi]
+}
+
+resource "aws_config_configuration_recorder" "rakulogi" {
+  name     = "rakulogi"
+  role_arn = aws_iam_role.r.arn
+}
+
+resource "aws_iam_role" "r" {
+  name = "my-awsconfig-role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "config.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy" "p" {
+  name = "my-awsconfig-policy"
+  role = aws_iam_role.r.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Action": "config:Put*",
+          "Effect": "Allow",
+          "Resource": "*"
+
+      }
+  ]
+}
+POLICY
+}

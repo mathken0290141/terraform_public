00572105e13d0ba01edb8f64210bc31ab779de0a
@_mathken <dummy@example.com>
2022-08-19 12:51:18 +0900

    バケット名の重複のエラーが出たため修正しました。

--- main.tf
+++ main.tf
@@ -11,20 +11,20 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
-resource "aws_cloudtrail" "foobar" {
-  name                          = "tf-trail-foobar"
-  s3_bucket_name                = aws_s3_bucket.foo.id
+resource "aws_cloudtrail" "rakulogi" {
+  name                          = "tf-trail-rakulogi"
+  s3_bucket_name                = aws_s3_bucket.rakulogi.id 
   s3_key_prefix                 = "prefix"
   include_global_service_events = false
 }
 
-resource "aws_s3_bucket" "foo" {
-  bucket        = "tf-test-trail"
+resource "aws_s3_bucket" "rakulogi" { //2つ目の""はS3のバケット名を表すので適宜変更してください
+  bucket        = "tf-rakulogi-trail"
   force_destroy = true
 }
 
-resource "aws_s3_bucket_policy" "foo" {
-  bucket = aws_s3_bucket.foo.id
+resource "aws_s3_bucket_policy" "rakulogi" {
+  bucket = aws_s3_bucket.rakulogi.id
   policy = <<POLICY
 {
     "Version": "2012-10-17",
@@ -36,7 +36,7 @@ resource "aws_s3_bucket_policy" "foo" {
               "Service": "cloudtrail.amazonaws.com"
             },
             "Action": "s3:GetBucketAcl",
-            "Resource": "${aws_s3_bucket.foo.arn}"
+            "Resource": "${aws_s3_bucket.rakulogi.arn}"
         },
         {
             "Sid": "AWSCloudTrailWrite",
@@ -45,7 +45,7 @@ resource "aws_s3_bucket_policy" "foo" {
               "Service": "cloudtrail.amazonaws.com"
             },
             "Action": "s3:PutObject",
-            "Resource": "${aws_s3_bucket.foo.arn}/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+            "Resource": "${aws_s3_bucket.rakulogi.arn}/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
             "Condition": {
                 "StringEquals": {
                     "s3:x-amz-acl": "bucket-owner-full-control"

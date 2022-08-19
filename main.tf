afc7793b404274a01cd12f2ca5e9097c971231e5
@_mathken <dummy@example.com>
2022-08-19 14:24:23 +0900

    有効化するためのパラメーターを追加しました。

--- main.tf
+++ main.tf
@@ -150,3 +150,73 @@ resource "aws_iam_role" "r" {
 }
 POLICY
 }
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/config_configuration_recorder_status
+
+resource "aws_config_configuration_recorder_status" "rakulogi" {
+  name       = aws_config_configuration_recorder.rakulogi.name
+  is_enabled = true
+  depends_on = [aws_config_delivery_channel.rakulogi]
+}
+
+resource "aws_iam_role_policy_attachment" "a" {
+  role       = aws_iam_role.r.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRole"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "awsconfig-rakulogi"
+}
+
+resource "aws_config_delivery_channel" "rakulogi" {
+  name           = "rakulogi"
+  s3_bucket_name = aws_s3_bucket.b.bucket
+}
+
+resource "aws_config_configuration_recorder" "rakulogi" {
+  name     = "rakulogi"
+  role_arn = aws_iam_role.r.arn
+}
+
+resource "aws_iam_role" "r" {
+  name = "rakulogi-awsconfig"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "config.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy" "p" {
+  name = "awsconfig-example"
+  role = aws_iam_role.r.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_s3_bucket.b.arn}",
+        "${aws_s3_bucket.b.arn}/*"
+      ]
+    }
+  ]
+}
+POLICY
+}

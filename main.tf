2cdef5d895a96ca68752cbb369ebf872a94650c3
@_mathken <dummy@example.com>
2022-08-19 12:45:24 +0900

    (AWS CloudTrail) ログ長期保管のための設定（証跡の作成）【MUST】の実行を行います

--- main.tf
+++ main.tf
@@ -7,30 +7,52 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_vpc" "test-vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "test-vpc"
-  }
+// (AWS CloudTrail) ログ長期保管のための設定（証跡の作成）【MUST】
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_cloudtrail" "foobar" {
+  name                          = "tf-trail-foobar"
+  s3_bucket_name                = aws_s3_bucket.foo.id
+  s3_key_prefix                 = "prefix"
+  include_global_service_events = false
 }
 
-// 追加コード
-resource "aws_subnet" "test-subnet-a" {
-  vpc_id     = aws_vpc.test-vpc.id
-  cidr_block = "10.0.0.0/28"
-  availability_zone = "ap-northeast-1a"
-
-  tags = {
-    Name = "test-subnet-a"
-  }
+resource "aws_s3_bucket" "foo" {
+  bucket        = "tf-test-trail"
+  force_destroy = true
 }
 
-resource "aws_subnet" "test-subnet-c" {
-  vpc_id     = aws_vpc.test-vpc.id
-  cidr_block = "10.0.0.16/28"
-  availability_zone = "ap-northeast-1c"
-
-  tags = {
-    Name = "test-subnet-c"
-  }
+resource "aws_s3_bucket_policy" "foo" {
+  bucket = aws_s3_bucket.foo.id
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailAclCheck",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:GetBucketAcl",
+            "Resource": "${aws_s3_bucket.foo.arn}"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "${aws_s3_bucket.foo.arn}/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
+    ]
+}
+POLICY
 }

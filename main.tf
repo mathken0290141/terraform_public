84f0daee0d3523ded514dc7972987516da23af7d
@_mathken <dummy@example.com>
2022-08-19 14:06:23 +0900

    (AWS Config) 有効化（レコーダーの作成）【MUST】

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-// "rakulogi"となっている箇所を適宜ご変更ください
+# "rakulogi"となっている箇所を適宜ご変更ください
 
-// (AWS CloudTrail) ログ長期保管のための設定（証跡の作成）【MUST】
-// https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudtrail
+# (AWS CloudTrail) ログ長期保管のための設定（証跡の作成）【MUST】
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudtrail
 
 data "aws_caller_identity" "current" {}
 
@@ -19,15 +19,15 @@ resource "aws_cloudtrail" "rakulogi" {
   s3_bucket_name                = aws_s3_bucket.rakulogi.id 
   s3_key_prefix                 = "prefix"
   include_global_service_events = false 
-  // (AWS CloudTrail) CloudTrail Insights の有効化【SHOULD】
-  //https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudtrail#insight_selector
+  # (AWS CloudTrail) CloudTrail Insights の有効化【SHOULD】
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudtrail#insight_selector
 
   insight_selector{
     insight_type="ApiCallRateInsight"
   }
 }
 
-resource "aws_s3_bucket" "rakulogi" { //2つ目の""はS3のバケット名を表すので適宜変更してください
+resource "aws_s3_bucket" "rakulogi" { 
   bucket        = "tf-rakulogi-trail"
   force_destroy = true
 }
@@ -62,6 +62,34 @@ resource "aws_s3_bucket_policy" "rakulogi" {
             }
         }
     ]
+}
+POLICY
+}
+
+# (AWS Config) 有効化（レコーダーの作成）【MUST】
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/config_configuration_recorder
+
+resource "aws_config_configuration_recorder" "rakulogi" {
+  name     = "rakulogi"
+  role_arn = aws_iam_role.r.arn
+}
+
+resource "aws_iam_role" "r" {
+  name = "awsconfig-rakulogi"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "config.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
 }
 POLICY
 }

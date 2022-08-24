3db5b1e2ed6ebab03bce6130c599bc4b7c4afcf8
@_mathken <dummy@example.com>
2022-08-24 19:27:20 +0900

    削除してみる

--- main.tf
+++ main.tf
@@ -192,9 +192,9 @@ resource "aws_iam_group_policy_attachment" "test-attach" {
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/accessanalyzer_analyzer
 
 
-resource "aws_organizations_organization" "root" {
-  aws_service_access_principals = ["access-analyzer.amazonaws.com"]
-}
+# resource "aws_organizations_organization" "root" {
+#   aws_service_access_principals = ["access-analyzer.amazonaws.com"]
+# }
 
 resource "aws_accessanalyzer_analyzer" "rakulogi" {
   depends_on = [aws_organizations_organization.root]

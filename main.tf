d1b3b6893b8e647107feaf068c69ca5c24806b27
@_mathken <dummy@example.com>
2022-08-30 06:29:02 +0900

    (AWS Budgets) 予算アラートの設定【SHOULD】

--- main.tf
+++ main.tf
@@ -310,7 +310,8 @@ resource "aws_budgets_budget" "rakulogi" {
   budget_type  = "COST"
   limit_amount = "100"
   limit_unit   = "USD"
-
+  time_unit         = "MONTHLY"
+  
     notification {
     comparison_operator        = "GREATER_THAN"
     threshold                  = 80
